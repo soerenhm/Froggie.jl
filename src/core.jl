ee505ebec371be71b9366c561d70ecb2804d55eb
Søren Hanghøj Møller <sorenhm0@gmail.com>
2021-09-13 23:43:17 -0400

    bugs + marginals

--- src/core.jl
+++ src/core.jl
@@ -4,6 +4,18 @@ has_wavelength_axis(A::AxisArray) = :λ in axisnames(A)
 
 is_frog_trace(A::AxisArray) = has_delay_axis(A) && (has_wavelength_axis(A) || has_frequency_axis(A))
 
+function frog_trace_type(A::AxisArray)
+  if !is_frog_trace(A)
+    nothing #Invalid
+  else
+    if has_frequency_axis(A)
+      nothing # some type
+    elseif has_wavelength_axis(A)
+      nothing # another type
+    end
+  end 
+end
+
 
 """
     frogtrace(data::AbstractMatrix, delays, wavelengths) -> AxisArray
@@ -30,28 +42,28 @@ Maps the wavelength axis to an angular frequency axis.
 function freqdomain(data::AxisArray)
   # check that data has λ axis
   has_wavelength_axis(data) || throw(ArgumentError("`data` does not containt a wavelength axis."))
-  
   ω = wavelen2angfreq.(data[Axis{:λ}])
 
   # To convert the spectral content from the spatial domain to the time domain,
   # multiply the trace  by multiply by dλ/dω ∝ 1/ω²
-    
   ξ = (ω[1] ./ ω).^2 |> ustrip
   new_data = reverse(data .* ξ', dims=2)
   ω = reverse(ω)
 
   AxisArray(
-    new_data;
-    t = data[Axis{:t}],
-    ω = enforce_unit(DefaultUnits.frequency, ω),
+    new_data,
+    AxisArrays.axes(data, Axis{:t}),
+    Axis{:ω}(enforce_unit(DefaultUnits.frequency, ω)),
   )
 end
 
 
-function frequencymarginal(A::AxisArray)
-  nothing
-end
+frequencymarginal(A::AxisArray) = has_frequency_axis(A) ? integrate(A, Axis{:t}) : nothing
 
 function delaymarginal(A::AxisArray)
-  nothing
+  if has_frequency_axis(A) 
+    integrate(A, Axis{:ω})
+  elseif has_wavelength_axis(A)
+    integrate(A, Axis{:λ})
+  end
 end
