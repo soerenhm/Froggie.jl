7a5eeff0ce490813a74d0081338bd1df11b44bc4
Søren Hanghøj Møller <sorenhm0@gmail.com>
2021-09-16 19:56:05 -0400

    test padding

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,8 +3,12 @@ using AxisArrays, Unitful
 
 @testset "Froggie" begin
 
-  @testset "Binning" begin
-    include("binning.jl")
+  # @testset "Binning" begin
+  #   include("binning.jl")
+  # end
+
+  @testset "Utils" begin
+    include("utils.jl")
   end
 
 end
